5693362849aec19a39bb11b770e064553d795eca
Sushi Sama <Phongsavanhx@gmail.com>
2019-03-02 13:38:05 -0700

    Updates

---
+++ precache-manifest.73d34d372f09e2ff05adef38c0ed3e26.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "e653d622ed16777f269272bedb38bf0d",
+    "url": "/Clicky-Game/static/media/mareep.e653d622.jpg"
+  },
+  {
+    "revision": "c8619095efdb29e16648",
+    "url": "/Clicky-Game/static/css/main.2fd1b610.chunk.css"
+  },
+  {
+    "revision": "61a0df336c60dbe48a52",
+    "url": "/Clicky-Game/static/js/runtime~main.61a0df33.js"
+  },
+  {
+    "revision": "646f3d2a74bef00b3a8f",
+    "url": "/Clicky-Game/static/js/2.646f3d2a.chunk.js"
+  },
+  {
+    "revision": "244449956f3b9696ed5867c1a65ffde0",
+    "url": "/Clicky-Game/static/media/Eevee.24444995.jpg"
+  },
+  {
+    "revision": "c21f404e1026b8098617b4218d2179e8",
+    "url": "/Clicky-Game/static/media/charmander.c21f404e.jpg"
+  },
+  {
+    "revision": "f0d778823cea8afc0aab97d69767771b",
+    "url": "/Clicky-Game/static/media/clefairy.f0d77882.jpg"
+  },
+  {
+    "revision": "966f07adba86e5473b17c215a4d2d1ab",
+    "url": "/Clicky-Game/static/media/cyndaquil.966f07ad.jpg"
+  },
+  {
+    "revision": "f64db2d6507be4855c1339b86006c3a1",
+    "url": "/Clicky-Game/static/media/duskull.f64db2d6.jpg"
+  },
+  {
+    "revision": "c8619095efdb29e16648",
+    "url": "/Clicky-Game/static/js/main.c8619095.chunk.js"
+  },
+  {
+    "revision": "c24ff226cfcce4279df5674b48dcc218",
+    "url": "/Clicky-Game/static/media/meltan.c24ff226.jpg"
+  },
+  {
+    "revision": "0630a9bcd0c381ab85d052704381ef13",
+    "url": "/Clicky-Game/static/media/mudkip.0630a9bc.jpg"
+  },
+  {
+    "revision": "020c3007c839402f5cbc21249aea9b68",
+    "url": "/Clicky-Game/static/media/oshawott.020c3007.jpg"
+  },
+  {
+    "revision": "c66af2a23be312707d4d7e4e958d215c",
+    "url": "/Clicky-Game/static/media/pikachu.c66af2a2.jpg"
+  },
+  {
+    "revision": "bbe68667e907df3ee6284acbfbe32246",
+    "url": "/Clicky-Game/static/media/piplup.bbe68667.jpg"
+  },
+  {
+    "revision": "884579bc8af01388be9a77211c45704e",
+    "url": "/Clicky-Game/static/media/squirtle.884579bc.jpg"
+  },
+  {
+    "revision": "e1225db9cf8e44ac5262d171bc2d1eef",
+    "url": "/Clicky-Game/static/media/zapdos.e1225db9.jpg"
+  },
+  {
+    "revision": "806093107d2ea2df7e3af4042d8cc37b",
+    "url": "/Clicky-Game/index.html"
+  }
+];
