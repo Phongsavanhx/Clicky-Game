5909a8ab04d9c05ffa5d07bb4cfe21cc727de95c
Sushi Sama <Phongsavanhx@gmail.com>
2019-03-10 16:13:15 -0700

    new commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Clicky-Game
+# NYT Google Books Search
+
+# Look up books to search using googles API!
+
+# Just type in the name of the book and watch it load all the references, click on a book to save and then enjoy!
---
+++ client/build/precache-manifest.2a18e6ac55c4a1363c0f4580c28b4901.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "815dec4ea3a21d87772c",
+    "url": "/static/js/main.815dec4e.chunk.js"
+  },
+  {
+    "revision": "9d405fab9f899e03fe6b",
+    "url": "/static/js/1.9d405fab.chunk.js"
+  },
+  {
+    "revision": "68e9866472abec87dac239e9dc3a7ddb",
+    "url": "/index.html"
+  }
+];
